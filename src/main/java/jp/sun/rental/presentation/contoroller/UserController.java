02439b79518141317627d5b2c7b5efb5a1840303
TogoNami <73asnrnavysou@gmail.com>
2026-01-28 15:18:35 +0900

    TOP画面を表示するメソッドを追加

--- src/main/java/jp/sun/rental/presentation/contoroller/UserController.java
+++ src/main/java/jp/sun/rental/presentation/contoroller/UserController.java
@@ -1,9 +1,32 @@
 package jp.sun.rental.presentation.contoroller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+	
+	//フィールド
+	
+	//コンストラクターインジェクション
 
+	
+	/* 「～/top」にGet通信されたとき、TOP画面を返す */
+	@GetMapping(value = "/top")
+	public String toTop() {
+		return "top";
+	}
+
+	/* 「～/login」にGet通信されたとき、ログイン画面を返す*/
+	
+	/* 「～/user/insert」にGet通信されたとき、ユーザー登録画面を返す*/
+	
+	/* 「～/user/insert」にPost通信されたとき、登録確認画面を返す
+	 * 登録情報を受け取る（ModelからFormで）
+	 * →Serviceに投げる
+	 * →結果を受け取る（成功したという情報）
+	 * →Viewに遷移する */
+	
+	/* 「～」にPost通信されたとき、ユーザー登録を実行し、登録完了画面を返す*/
 	
 }
