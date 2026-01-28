1866061315ffa4830b1f391d730bd4e14eea8720
yahiro <syouta.yahiro@gmail.com>
2026-01-28 14:29:46 +0900

    初期設定変更、エンティティ追加

---
+++ src/main/java/jp/sun/rental/domain/entity/MemberEntity.java
@@ -0,0 +1,19 @@
+package jp.sun.rental.domain.entity;
+
+import lombok.Data;
+
+@Data
+public class MemberEntity {
+
+	private int userId;
+	
+	private int card;
+	
+	private int userPoint;
+	
+	private String address;
+	
+	private String post;
+	
+	private String plan;
+}
