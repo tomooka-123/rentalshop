07053ea74623a744ba85fffc267985c697509fc7
TogoNami <73asnrnavysou@gmail.com>
2026-02-02 14:21:15 +0900

    ユーザー登録処理が正常に動くように修正
    registメソッドを2つから1つにした
    userInsertFormをセッション管理にした
    例外ハンドラーを追加した
    登録完了時にセッション破棄するようにした

--- src/main/java/jp/sun/rental/presentation/form/UserInsertForm.java
+++ src/main/java/jp/sun/rental/presentation/form/UserInsertForm.java
@@ -2,6 +2,7 @@ package jp.sun.rental.presentation.form;
 
 import java.io.Serializable;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
@@ -9,22 +10,30 @@ public class UserInsertForm implements Serializable {
 	
 	private String userId;
 	
+	@NotEmpty
 	private String userName;
 	
+	@NotEmpty
 	private String name;
 	
+	@NotEmpty
 	private String email;
 	
+	@NotEmpty
 	private String tell;
 	
+	@NotEmpty
 	private String post;
 	
+	@NotEmpty
 	private String address;
 	
 	private String plan;
 	
+	@NotEmpty
 	private String card;	
 	
+	@NotEmpty
 	private String password;
 	
 	private String authority;
