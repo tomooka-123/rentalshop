9637117afba04afcc402d0697b2c41c81962bac6
yahiro <syouta.yahiro@gmail.com>
2026-02-13 13:14:31 +0900

    レンタル希望者の住所などを登録するメソッドを追加

--- src/main/java/jp/sun/rental/domain/repository/RentalRepository.java
+++ src/main/java/jp/sun/rental/domain/repository/RentalRepository.java
@@ -7,13 +7,18 @@ import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
 
 import jp.sun.rental.domain.entity.CartItemEntity;
+import jp.sun.rental.domain.entity.MemberEntity;
+import jp.sun.rental.domain.entity.RentalAddressEntity;
 import jp.sun.rental.domain.entity.RentalHistoryEntity;
+import jp.sun.rental.domain.entity.UserEntity;
+import jp.sun.rental.infrastructure.mapper.RentalAddressRowMapper;
 import jp.sun.rental.infrastructure.mapper.RentalResultSetExtractor;
 
 @Repository
 public class RentalRepository {
 
 	private ResultSetExtractor<List<RentalHistoryEntity>> historyExtractor = new RentalResultSetExtractor();
+	private RentalAddressRowMapper rentalAddressRowMapper = new RentalAddressRowMapper();
 	private JdbcTemplate jdbcTemplate;
 	
 	public RentalRepository(JdbcTemplate jdbcTemplate) {
@@ -45,6 +50,24 @@ public class RentalRepository {
 		return historyEntityList;
 	}
 	
+	//ユーザーIDからそのユーザーのレンタル希望履歴の住所などを取得
+	public List<RentalAddressEntity> getRentalAddressByUserId(int userId)throws Exception{
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("SELECT");
+		sb.append(" ra.rental_id, ra.address, ra.name, ra.tell");
+		sb.append(" FROM rental_address ra");
+		sb.append(" LEFT OUTER JOIN rental r");
+		sb.append(" ON ra.rental_id = r.rental_id");
+		sb.append(" WHERE r.user_id = ?");
+		
+		String sql = sb.toString();
+		
+		List<RentalAddressEntity> addressEntityList = jdbcTemplate.query(sql, rentalAddressRowMapper, userId);
+		
+		return addressEntityList;
+	}
+	
 	//レンタル履歴登録
 	public int registRental(int userid)throws Exception{
 		StringBuilder sb = new StringBuilder();
@@ -73,6 +96,22 @@ public class RentalRepository {
 		rowItems = jdbcTemplate.update(sql, rentalId, cartItemEntity.getItemId());
 		
 		return rowItems;
+	}
+	
+	//レンタルした人の現在の住所などを登録
+	public int registRentalAddress(int rentalId, UserEntity userEntity, MemberEntity memberEntity)throws Exception{
+		StringBuilder sb = new StringBuilder();
+		
+		int rowAddress = 0;
+		
+		sb.append("INSERT INTO rental_address (rental_id, address, name, tell)");
+		sb.append(" VALUES (?, ?, ?, ?)");
+		
+		String sql = sb.toString();
+		
+		rowAddress = jdbcTemplate.update(sql, rentalId, memberEntity.getAddress(), memberEntity.getName(), userEntity.getTell());
+		
+		return rowAddress;
 	}
 	
 	//返却フラグ切り替え
